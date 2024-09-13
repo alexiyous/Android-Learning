f5da0e8547811959e59c4373af4c4939c69295b7
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2024-09-13 23:52:08 +0700

    Work Scheduler with weather, One time and repeating

--- Android-Fundamental/Github/app/src/main/java/com/example/github/data/retrofit/ApiConfig.kt
+++ Android-Fundamental/Github/app/src/main/java/com/example/github/data/retrofit/ApiConfig.kt
@@ -3,7 +3,6 @@ package com.example.github.data.retrofit
 import com.example.github.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
---
+++ Android-Fundamental/MyWorkManager/app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply true
+}
+
+android {
+    namespace = "com.example.myworkmanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myworkmanager"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "API_KEY", "\"43c8c751da4c56fc1f05ccae693a514b\"")
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.kotlin.codegen)
+
+    // WorkManager
+    implementation("androidx.work:work-runtime:2.8.1")
+    // Async HTTP
+    implementation("com.loopj.android:android-async-http:1.4.10")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
