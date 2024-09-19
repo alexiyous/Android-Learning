5aa8068ae64b612e088ee04a21ed5273a52ac301
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2024-09-19 16:37:07 +0700

    format penulisan untuk uang, number, date, currency

--- Android Intermediate/LatihanLocalizationPadaTeksStarter/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ Android Intermediate/LatihanLocalizationPadaTeksStarter/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        setupData()
     }
 
     private fun setupAction() {
@@ -39,5 +40,24 @@ class MainActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun setupData() {
+        val data = RemoteDataSource(this)
+        val product = data.getDetailProduct()
+
+        product.apply {
+            binding.apply {
+                previewImageView.setImageResource(image)
+                nameTextView.text = name
+                storeTextView.text = store
+                colorTextView.text = color
+                sizeTextView.text = size
+                descTextView.text = desc
+                priceTextView.text = price.withCurrencyFormat()
+                dateTextView.text = getString(R.string.dateFormat, date.withDateFormat())
+                ratingTextView.text = getString(R.string.ratingFormat, rating.withNumberingFormat(), countRating.withNumberingFormat())
+            }
+        }
     }
 }
