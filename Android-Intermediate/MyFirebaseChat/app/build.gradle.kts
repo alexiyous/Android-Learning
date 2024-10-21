494d1dac4feea5708b5d68a40c3c0e8a7e5f90c9
Alexius Andrianno Alfa Renanta <alexiusrenanta@gmail.com>
2024-10-21 11:25:18 +0700

    Realtime Database with chat message feature

--- Android-Intermediate/MyFirebaseChat/app/build.gradle.kts
+++ Android-Intermediate/MyFirebaseChat/app/build.gradle.kts
@@ -51,12 +51,17 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
 
+    // Firebase UI
+    implementation(libs.firebase.ui.database)
+    implementation(libs.glide)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
